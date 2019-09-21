4ef69e2c6d8aa12be76698167863be095b9e67d6
Arthur <arthurcs426@gmail.com>
2019-09-21 12:06:33 -0700

    all routes dynamically passing

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const db = require('./db');
 const { User, Department } = db.models;
+const { pluralize } = require('inflection');
 
 const app = express();
 
@@ -8,56 +9,32 @@ module.exports = app;
 
 app.use(express.json());
 
-Object.entries(db.conn.models).forEach()
+Object.entries(db.conn.models).forEach(([name, model]) => {
+  const route = `/api/${pluralize(name)}`;
 
-app.get('/api/users', (req, res, next) => {
-  User.findAll()
-    .then(users => res.send(users))
-    .catch(next);
-});
+  app.get(route, (req, res, next) => {
+    model.findAll()
+      .then(items => res.send(items))
+      .catch(next);
+  });
 
-app.post('/api/users', (req, res, next) => {
-  User.create(req.body)
-    .then(user => res.status(201).send(user))
-    .catch(next);
-});
+  app.post(route, (req, res, next) => {
+    model.create(req.body)
+      .then(item => res.status(201).send(item))
+      .catch(next);
+  });
 
-app.put('/api/users/:id', (req, res, next) => {
-  User.findByPk(req.params.id)
-    .then(user => user.update(req.body))
-    .then(user => res.send(user))
-    .catch(next);
-});
+  app.put(`${route}/:id`, (req, res, next) => {
+    model.findByPk(req.params.id)
+      .then(item => item.update(req.body))
+      .then(item => res.send(item))
+      .catch(next);
+  });
 
-app.delete('/api/users/:id', (req, res, next) => {
-  User.findByPk(req.params.id)
-    .then(user => user.destroy())
-    .then(() => res.sendStatus(204))
-    .catch(next);
-});
-
-app.get('/api/departments', (req, res, next) => {
-  Department.findAll()
-    .then(departments => res.send(departments))
-    .catch(next);
-});
-
-app.post('/api/departments', (req, res, next) => {
-  Department.create(req.body)
-    .then(department => res.status(201).send(department))
-    .catch(next);
-});
-
-app.put('/api/departments/:id', (req, res, next) => {
-  Department.findByPk(req.params.id)
-    .then(department => department.update(req.body))
-    .then(department => res.send(department))
-    .catch(next);
-});
-
-app.delete('/api/departments/:id', (req, res, next) => {
-  Department.findByPk(req.params.id)
-    .then(department => department.destroy())
-    .then(() => res.sendStatus(204))
-    .catch(next);
+  app.delete(`${route}/:id`, (req, res, next) => {
+    model.findByPk(req.params.id)
+      .then(item => item.destroy())
+      .then(() => res.sendStatus(204))
+      .catch(next);
+  });
 });
